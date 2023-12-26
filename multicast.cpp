e1256050efda47d39fd1fc2b15ca46c1ed47e579
Saikat Haldar <saikat_haldar@hotmail.com>
2023-12-25 23:34:11 -0800

    added FSM multicast

---
+++ multicast.cpp
@@ -0,0 +1,105 @@
+/* 
+
+multicast.c
+
+The following program sends or receives multicast packets. If invoked
+with one argument, it sends a packet containing the current time to an
+arbitrarily chosen multicast group and UDP port. If invoked with no
+arguments, it receives and prints these packets. Start it as a sender on
+just one host and as a receiver on all the other hosts
+
+*/
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdio.h>
+
+#define HOST_INTERFACE_CARD "192.168.1.1" // inc case of multiple network card. This tells which internet interface to use for multicast.
+#define MULTI_HOST_ENV 
+#define EXAMPLE_PORT 6000
+#define EXAMPLE_GROUP "239.0.0.1"
+
+main(int argc)
+{
+   struct sockaddr_in addr;
+   int addrlen, sock, cnt;
+   struct ip_mreq mreq;
+   char message[50];
+
+   /* set up socket */
+   sock = socket(AF_INET, SOCK_DGRAM, 0);
+   if (sock < 0) 
+   {
+     perror("socket");
+     exit(1);
+   }
+   bzero((char *)&addr, sizeof(addr));
+   addr.sin_family = AF_INET;
+   addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   addr.sin_port = htons(EXAMPLE_PORT);
+   addrlen = sizeof(addr);
+
+   if (argc > 1) 
+   {  //****************************** Client ***************************************
+      /* send data */
+      addr.sin_addr.s_addr = inet_addr(EXAMPLE_GROUP);
+      while (1) 
+      {
+         time_t t = time(0);
+         sprintf(message, "time is %-24.24s", ctime(&t));
+         printf("sending: %s\n", message);
+         cnt = sendto(sock, message, sizeof(message), 0,(struct sockaddr *) &addr, addrlen);
+         if (cnt < 0) 
+         {
+            perror("sendto");
+            exit(1);
+         }
+         sleep(5);
+      }
+   } 
+   else 
+   {  //******************************* server **************************************
+
+      /* receive receive */
+      if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
+      {        
+         perror("bind");
+	      exit(1);
+      }    
+      mreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);         
+      mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
+      if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) 
+      {
+         perror("setsockopt mreq");
+         exit(1);
+      }  
+      #ifdef MULTI_HOST_ENV  //in case of multiple network card or multihost
+      mreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);         
+      mreq.imr_interface.s_addr = inet_addr(HOST_INTERFACE_CARD);         
+      if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP|IP_MULTICAST_IF, &mreq, sizeof(mreq)) < 0) 
+      {
+         perror("setsockopt mreq");
+         exit(1);
+      }  
+      #endif         
+      while (1) 
+      {
+ 	      cnt = recvfrom(sock, message, sizeof(message), 0, (struct sockaddr *) &addr, &addrlen);
+	      if (cnt < 0) 
+         {
+	         perror("recvfrom");
+	         exit(1);
+	      } 
+         else if (cnt == 0) 
+         {
+ 	         break;
+	      }
+	      printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
+        }
+   }
+}
